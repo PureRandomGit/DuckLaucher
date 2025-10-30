9326523e23a9be49a5b83d8da8843268a7e7144c
purerandom <samuelks82406@gmail.com>
2025-10-30 13:15:22 -0400

    Added Library

---
+++ UCF_RSLK_Library-1.5/README.md
@@ -0,0 +1,60 @@
+{#mainpage}
+=================
+
+# Introduction
+
+
+This [Energia] (https://energia.nu/) library provides a set of classes and functions to make using the [Texas Instrument's Robotic System Learning Kit] (http://www.ti.com/rslk) simple. @n Go to http://www.ti.com/rslk to see the user guides, view curriculum and find the link to order.
+
+# Installation
+
+1. Download and install the latest version of Arduino IDE (1.8.13). https://www.arduino.cc/en/software/
+
+2. Add the URL http://s3.amazonaws.com/energiaUS/packages/package_energia_index.json to your File > Preferences > Additional Board Manager URLs. Also check the box for show line numbers if you like that and adjust your font size if you would like it bigger.
+
+3. Go to Tools > Boards > Boards Manager and scroll to the bottom of the list with Energia MSP432 and install 5.29.1
+
+4. After the install completes, You need to select the board and COM port in Arduino IDE. Go to Tool > Boards and now you should see the "Energia MSP432 Red Boards" > "Red LaunchPad MSP432P401R EMT" and make sure this is selected.
+
+5. You also should select the correct COM port. This step can be done after you plug your LaunchPad to your computer over USB and you install your LaunchPad drivers (done in later step using RSLK debug tool GUI). Go to Tools > COM port to chose from available options. The LaunchPad populates two COM ports. MacOS users will see Port 001 and 004 populated, please use Port 1. Windows users can verify their COM port by going to Device Manager and finding the XDS110 UART.
+
+6. Add the latest release of the RSLK robot library that is downloadable from the releases section of this page. Download the sorces.zip file. Go to Sketch > Include Library > add .ZIP library. Select the file path of the downloaded zip library and click okay. It should say library added and you can check this by going to File > Examples > TI-Robot-Lib and see the example code provided in the library.
+
+7. Go to Tools > Manage Libraries.. and search for and install "BNO055 by Robert Bosch GMBH"
+
+# Using This Library
+
+## Library Documentation
+Library documentation is hosted on Github Pages at https://fcooper.github.io/Energia-RSLK-Library/
+
+## SimpleRSLK (Recommended)
+
+
+The SimpleRSLK.h defines a set of easy to use functions for the RSLK. Internally it calls the classes and functions mentioned below.
+
+## Peripheral Specific Library/Function
+
+Functions and classes have been created for each of the peripherals used on the RSLK Max.
+
+- Bump_Switch provides an easy to use class for a a single bump switch.
+- Romi_Motor_Power provides an easy to use class for a single motor.
+- QTRSensors provides an easy to use class for the line sensor. Provided by Pololu.
+- GP2Y0A21_Sensor provides an easy to use class for the GP2Y0A21 IR distance sensor.
+- Encoder.h provides an easy to use set of functions for the two onboard Encoders.
+
+### Pins and Other Important Defines
+
+RSLK_Pins.h provides an important list of pins definitions for the RSLK.
+
+## Useful Documentation
+- [TI RSLK] (http://www.ti.com/rslk) homepage to learn more, view user guide, curriculum and a link to order.
+- [RSLK Max Schematic] (https://www.pololu.com/file/0J1670/ti-rslk-max-chassis-board-v1.0-schematic.pdf)
+- [Full Chassis Board Pin Diagram] (https://www.pololu.com/file/0J1695/ti_rslk_max_chassis_board_pinout.pdf)
+
+### Links to Parts
+- [Left Bump Switch Assembly](https://www.pololu.com/product/3673)
+- [Right Bump Switch Assembly](https://www.pololu.com/product/3674)
+- [Line Sensor](https://www.pololu.com/product/3672)
+- [Motor Board] (https://www.pololu.com/product/3671)
+- [Gearmotor and Encoder] (https://www.pololu.com/product/3675)
+
---
+++ UCF_RSLK_Library-1.5/examples/Robot_Challenge/Robot_Challenge.ino
@@ -0,0 +1,306 @@
+#include "SimpleRSLK.h"
+#include "BNO055_support.h"		//Contains the bridge code between the API and Arduino
+#include <Wire.h>
+
+struct bno055_t myBNO;
+struct bno055_euler myEulerData; //Structure to hold the Euler data
+
+float wheelDiameter = 2.5;      // Diameter of Romi wheels in inches
+int cntPerRevolution = 360;   // Number of encoder (rising) pulses every time the wheel turns completely
+
+float initialHeading;
+
+uint16_t sensorVal[LS_NUM_SENSORS];
+uint16_t sensorCalVal[LS_NUM_SENSORS];
+uint16_t sensorMaxVal[LS_NUM_SENSORS] = {2500, 2500, 2500, 2500, 2500, 2500, 2500, 2500};
+uint16_t sensorMinVal[LS_NUM_SENSORS] = {805, 1047, 759, 955, 790, 1154, 931, 1245};
+
+#define GOAL 3500       // value of normalized light sensor to follow
+#define BASE_SPEED 10         // Default speed of the robot
+#define TURN_SPEED 10         // Default turn speed
+#define P_FOLLOW 0.01         // Proportional constant for line following
+#define P_GYRO_DRIVE 5.0      // Proportional constant for gyro heading following
+#define TURN_COUNT 350        // Counts to turn
+#define DESIRED_HEADING 0     // Heading to return on (figure it out)
+
+float crashedCount;
+int currentCount;
+unsigned long lastTime = 0;
+String btnMsg = " ";
+
+typedef enum State
+{
+  START,
+  FOLLOW,
+  CRASH,
+  TURN_SIMPLE,
+  TURN_GYRO,
+  RESET_TURN,
+  FOLLOW_HOME,
+  STRAIGHT_HOME,
+  GYRO_HOME,
+  DONE,
+  SLEEP
+} State;
+
+State state = START;     // Initial state
+
+void setup()
+{
+  // Initialize I2C communication
+  Wire.begin();
+
+  // Initialization of the BNO055
+  BNO_Init(&myBNO); //Assigning the structure to hold information about the device
+
+  // Configuration to NDoF mode
+  bno055_set_operation_mode(OPERATION_MODE_NDOF);
+
+  delay(1);       // Wait for gyro to settle
+  Serial.begin(115200);
+
+  setupRSLK();
+  clearMinMax(sensorMinVal,sensorMaxVal);
+
+  // reset encoders
+	resetLeftEncoderCnt();
+	resetRightEncoderCnt();
+
+  setupWaitBtn(LP_LEFT_BTN);    // Left botton on the Launchpad
+  setupLed(RED_LED);            // Use red led to signal waiting for button
+
+  /* Initialize motors */
+	setMotorDirection(BOTH_MOTORS,MOTOR_DIR_FORWARD);
+	enableMotor(BOTH_MOTORS);
+	setMotorSpeed(BOTH_MOTORS,0);
+
+  // Read initial heading
+  delay(1000);
+  delay(1000);
+  bno055_read_euler_hrp(&myEulerData);			//Update Euler data into the structure
+  initialHeading = float(myEulerData.h) / 16.00;
+  Serial.print("Initial Heading(Yaw): ");				//To read out the Heading (Yaw)
+  Serial.println(initialHeading);
+
+  enableMotor(BOTH_MOTORS);
+}
+
+// MAIN LOOP // 
+
+void loop()
+{
+  delay(50);      // Don't loop too fast
+
+  if ((millis() - lastTime) >= 10) // Print state at 10hz
+  {
+    Serial.println(state);
+    lastTime = millis();
+  }
+
+  // Run Current State
+  switch (state) {
+    case START: 
+      Serial.println("Start");
+	    /* Wait until button is pressed to start robot */
+	    waitBtnPressed(LP_LEFT_BTN,"\nPush left button on Launchpad to start challenge.\n",RED_LED);
+      state = FOLLOW;
+    break;
+
+    // Follow the line
+    case FOLLOW:
+      follow(P_FOLLOW,BASE_SPEED);
+      if (crashed()) {
+         state = CRASH;
+      }
+    break;
+
+    case CRASH:
+      setMotorSpeed(LEFT_MOTOR, 0);
+      setMotorSpeed(RIGHT_MOTOR, 0);
+      crashedCount = getEncoderRightCnt();
+      state = TURN_SIMPLE;
+    break;
+
+    case TURN_SIMPLE:
+      setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_BACKWARD);
+      setMotorSpeed(LEFT_MOTOR,BASE_SPEED);
+      setMotorSpeed(RIGHT_MOTOR,BASE_SPEED);
+      currentCount = getEncoderRightCnt();
+      Serial.print("Ticks: ");
+      Serial.println(currentCount);
+      if ( currentCount > crashedCount + TURN_COUNT) {
+        setMotorSpeed(LEFT_MOTOR,0);
+        setMotorSpeed(RIGHT_MOTOR,0);
+        state = RESET_TURN;
+      }
+    break;
+
+    case TURN_GYRO:
+      if (turnTo(90,TURN_SPEED)) {
+        state = RESET_TURN;
+      }
+    break;
+
+    case RESET_TURN:
+      setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_FORWARD);
+      setMotorDirection(LEFT_MOTOR,MOTOR_DIR_FORWARD);
+      // reset encoders
+	    resetLeftEncoderCnt();
+	    resetRightEncoderCnt();
+      state = STRAIGHT_HOME;
+    break;
+
+    case GYRO_HOME:
+      if (driveToDistanceHeading(P_GYRO_DRIVE,12,0,BASE_SPEED)) {
+        state = DONE;
+      };
+    break;
+
+    case STRAIGHT_HOME:
+      setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_FORWARD);
+      setMotorDirection(LEFT_MOTOR,MOTOR_DIR_FORWARD);
+      setMotorSpeed(LEFT_MOTOR, BASE_SPEED+20);
+      setMotorSpeed(RIGHT_MOTOR, BASE_SPEED+20);
+    break;
+
+    case FOLLOW_HOME:
+      follow(P_FOLLOW+.01,BASE_SPEED+10);   // Follow a little faster on the way back
+    break;
+
+    case DONE:	/* Halt motors */
+      Serial.println("DONE");
+	    disableMotor(BOTH_MOTORS);
+      state = SLEEP;
+    break;
+
+    case SLEEP:
+    break;
+
+  } // end state
+}
+
+boolean crashed()
+{
+ return (isBumpSwitchPressed(2) || isBumpSwitchPressed(3));
+}
+
+void follow(float myP, int myBaseSpeed)
+{
+	/* Valid values are either:
+	 *  DARK_LINE  if your floor is lighter than your line
+	 *  LIGHT_LINE if your floor is darker than your line
+	 */
+	uint8_t lineColor = DARK_LINE;
+
+	readLineSensor(sensorVal);
+
+  /*
+   * Take current sensor values and adjust using previous calibration values
+   * Output: sensorCalVal
+   */
+	readCalLineSensor(sensorVal,
+					  sensorCalVal,
+					  sensorMinVal,
+					  sensorMaxVal,
+					  lineColor);
+
+	uint32_t linePos = getLinePosition(sensorCalVal,lineColor);
+
+  /* use PID algorithm to calculate speed delta */
+  int error = linePos - GOAL;
+  int motor_speed_delta = myP * error;
+    Serial.print(linePos);
+    Serial.print(", ");
+    Serial.print(error);
+    Serial.print(", ");
+    Serial.print(motor_speed_delta);
+    Serial.println();
+
+  /* add and subtract delta from left and right sides.  Cap motors at 100 */
+  int temp = myBaseSpeed + motor_speed_delta;
+  int left_motor_speed = (temp > 100) ? 100 : temp; 
+  temp = myBaseSpeed - motor_speed_delta;
+  int right_motor_speed = (temp < 0) ? 0 : temp; 
+
+  setMotorSpeed(LEFT_MOTOR, left_motor_speed);
+  setMotorSpeed(RIGHT_MOTOR, right_motor_speed);
+}
+
+/*
+ * Drive distance
+ */
+boolean driveToDistanceHeading(float myP, float inches, int desiredHeading, int speed) {
+  uint16_t ticks = distanceToEncoder(wheelDiameter, cntPerRevolution, inches);
+
+  int headingError = calculateDifferenceBetweenAngles(desiredHeading, getCurrentRealtiveHeadingToStart());
+  Serial.print("Drive: Heading Error");
+  Serial.println(headingError);
+  int adjustSpeed = headingError * myP;
+  adjustSpeed = constrain(adjustSpeed,-10,10);
+  /* Set motor speed */
+  setMotorDirection(LEFT_MOTOR,MOTOR_DIR_FORWARD);   
+  setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_FORWARD);
+	setMotorSpeed(LEFT_MOTOR,constrain(speed+adjustSpeed,0,100));
+  setMotorSpeed(RIGHT_MOTOR,constrain(speed-adjustSpeed,0,100));
+
+  Serial.print("Left motor: "); Serial.print(speed+adjustSpeed);
+  Serial.print(" Right motor: "); Serial.println(speed-adjustSpeed);
+
+  int totalCount = (getEncoderLeftCnt() + getEncoderRightCnt()) / 2;
+	if (totalCount > ticks) {
+	  setMotorSpeed(BOTH_MOTORS,0);  // Halt motors
+    return true;
+	}
+  return false;
+}
+
+/*
+ * Turn degrees
+ */
+boolean turnTo(int degrees, int speed) {
+  
+  int headingError = calculateDifferenceBetweenAngles(degrees, getCurrentRealtiveHeadingToStart());
+
+
+  if (abs(headingError) < 2) {
+     setMotorSpeed(BOTH_MOTORS,0);  // Halt motors
+      return true;
+  }
+
+  Serial.print("Turn: Heading Error: ");				//To read out the Heading (Yaw)
+  Serial.println(headingError);		
+
+  if (headingError >= 0) {
+      Serial.println("TURN Right");
+      setMotorDirection(LEFT_MOTOR,MOTOR_DIR_FORWARD);   
+      setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_BACKWARD);
+      setMotorSpeed(BOTH_MOTORS,speed);
+	} else {
+      setMotorDirection(LEFT_MOTOR,MOTOR_DIR_BACKWARD);   
+      setMotorDirection(RIGHT_MOTOR,MOTOR_DIR_FORWARD);
+      setMotorSpeed(BOTH_MOTORS,speed);
+  }
+
+  return false;
+}
+
+int getCurrentRealtiveHeadingToStart(){
+  bno055_read_euler_hrp(&myEulerData);			//Update Euler data into the structure
+  float difference = (float(myEulerData.h) / 16.00) - initialHeading;
+  return ((int)difference + 360) % 360;
+} 
+
+/*
+ * calculateDifferenceBetweenAngles
+ * ---------------------------------
+ * Return the difference between two angles in a 0-360 system
+ * - returns +-179
+ */
+int calculateDifferenceBetweenAngles(int angle1, int angle2) {
+   int delta;
+
+    delta = (angle1 - angle2 + 360) % 360;
+       if (delta > 180) delta = delta - 360;
+
+     return delta;
+}
